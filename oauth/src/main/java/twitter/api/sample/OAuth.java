c046cc1bb52969844d1da50465ec0177c90a61ce
YujiSoftware <yuji.software+github@gmail.com>
2023-01-18 21:02:07 +0900

    Add token refresh sample code

--- oauth/src/main/java/twitter/api/sample/OAuth.java
+++ oauth/src/main/java/twitter/api/sample/OAuth.java
@@ -1,8 +1,12 @@
 package twitter.api.sample;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
@@ -12,13 +16,18 @@ import com.github.scribejava.core.pkce.PKCECodeChallengeMethod;
 import com.twitter.clientlib.auth.TwitterOAuth20Service;
 
 public class OAuth {
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, URISyntaxException {
+    public static void main(String[] args)
+            throws IOException, ExecutionException, InterruptedException, URISyntaxException {
+        Properties properties = new Properties();
+        try (Reader reader = Files.newBufferedReader(Path.of("../twitter.properties"))) {
+            properties.load(reader);
+        }
+
         TwitterOAuth20Service service = new TwitterOAuth20Service(
-                System.getenv("TWITTER_OAUTH2_CLIENT_ID"),
-                System.getenv("TWITTER_OAUTH2_CLIENT_SECRET"),
-                System.getenv("TWITTER_OAUTH2_CALLBACK"),
-                "tweet.read tweet.write users.read"
-        );
+                properties.getProperty("TWITTER_OAUTH2_CLIENT_ID"),
+                properties.getProperty("TWITTER_OAUTH2_CLIENT_SECRET"),
+                properties.getProperty("TWITTER_OAUTH2_CALLBACK"),
+                "tweet.read tweet.write users.read offline.access");
 
         Scanner in = new Scanner(System.in, StandardCharsets.UTF_8);
         System.out.println("Fetching the Authorization URL...");
@@ -31,7 +40,7 @@ public class OAuth {
         String authorizationUrl = service.getAuthorizationUrl(pkce, secretState);
 
         System.out.println("Go to the Authorization URL and authorize your App:\n" +
-            authorizationUrl + "\nAfter that paste the authorization code here\n>>");
+                authorizationUrl + "\nAfter that paste the authorization code here\n>>");
         String code = in.nextLine();
 
         System.out.println("\nTrading the Authorization Code for an Access Token...");
--- refresh/src/main/java/twitter/api/sample/Refresh.java
+++ refresh/src/main/java/twitter/api/sample/Refresh.java
@@ -2,6 +2,7 @@ package twitter.api.sample;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
@@ -9,10 +10,8 @@ import java.util.Properties;
 import com.twitter.clientlib.ApiException;
 import com.twitter.clientlib.TwitterCredentialsOAuth2;
 import com.twitter.clientlib.api.TwitterApi;
-import com.twitter.clientlib.model.TweetCreateRequest;
-import com.twitter.clientlib.model.TweetCreateResponse;
 
-public class Tweet {
+public class Refresh {
     public static void main(String[] args) throws ApiException, IOException {
         Properties properties = new Properties();
         try (Reader reader = Files.newBufferedReader(Path.of("../twitter.properties"))) {
@@ -25,13 +24,15 @@ public class Tweet {
                 properties.getProperty("TWITTER_OAUTH2_ACCESS_TOKEN"),
                 properties.getProperty("TWITTER_OAUTH2_REFRESH_TOKEN"));
         TwitterApi api = new TwitterApi(credentials);
+        api.addCallback(token -> {
+            properties.setProperty("TWITTER_OAUTH2_ACCESS_TOKEN", token.getAccessToken());
+            properties.setProperty("TWITTER_OAUTH2_REFRESH_TOKEN", token.getRefreshToken());
+        });
 
-        String text = "Hello world!";
+        api.refreshToken();
 
-        TweetCreateRequest request = new TweetCreateRequest().text(text);
-
-        TweetCreateResponse result = api.tweets().createTweet(request).execute();
-
-        System.out.println(result);
+        try (Writer writer = Files.newBufferedWriter(Path.of("../twitter.properties"))) {
+            properties.store(writer, "");
+        }
     }
 }
